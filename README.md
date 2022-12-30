8b00dd2417b6ae4885405083ba76d38d20b13235
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2022-12-30 14:41:59 +0700

    Add first files

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # spaCy-PyThaiNLP
 PyThaiNLP For spaCy
+
+Work in processing...
+
+## Install
+
+> pip install https://github.com/PyThaiNLP/spaCy-PyThaiNLP/archive/refs/heads/main.zip
+
+## How to use
+
+```python
+import spacy
+from spacy_pythainlp import *
+
+nlp = spacy.blank("th")
+# Segment the Doc into sentences
+nlp.add_pipe(
+   "th_sents", 
+)
+
+data=nlp("ผมเป็นคนไทย   แต่มะลิอยากไปโรงเรียนส่วนผมจะไปไหน  ผมอยากไปเที่ยว")
+print(list(list(data.sents)))
+# output: [ผมเป็นคนไทย   แต่มะลิอยากไปโรงเรียนส่วนผมจะไปไหน  , ผมอยากไปเที่ยว]
+```
+
+## License
+
+```
+   Copyright 2016-2023 PyThaiNLP Project
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+```
---
+++ setup.py
@@ -0,0 +1,54 @@
+from setuptools import find_packages, setup
+
+requirements = [
+    "pythainlp>=3.1.0",
+    "spacy>=3.0"
+]
+
+with open("README.md", "r") as f:
+    readme = f.read()
+
+
+setup(
+    name="spacy-pythainlp",
+    version="0.1dev0",
+    description="PyThaiNLP For spaCy",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="Wannaphong Phatthiyaphaibun",
+    author_email="wannaphong@yahoo.com",
+    url="https://github.com/PyThaiNLP/spaCy-PyThaiNLP",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    # test_suite="tests",
+    python_requires=">=3.7",
+    include_package_data=True,
+    install_requires=requirements,
+    license="Apache Software License 2.0",
+    zip_safe=False,
+    keywords=[
+        "pythainlp",
+        "NLP",
+        "natural language processing",
+        "text analytics",
+        "text processing",
+        "localization",
+        "computational linguistics",
+        "ThaiNLP",
+        "Thai NLP",
+        "Thai language",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: Thai",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Text Processing",
+        "Topic :: Text Processing :: General",
+        "Topic :: Text Processing :: Linguistic",
+    ],
+    project_urls={
+        "Source Code": "https://github.com/PyThaiNLP/spaCy-PyThaiNLP",
+        "Bug Tracker": "https://github.com/PyThaiNLP/spaCy-PyThaiNLP/issues",
+    },
+)
